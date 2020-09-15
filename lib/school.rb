206c63da4e2d9342e94bab04f10cd5d72d2e4ce6
jrwatson94 <j.watsonreid@gmail.com>
2020-09-15 18:35:37 -0400

    lab completed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require "pry"
+class School 
+    attr_accessor :name, :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name,grade)
+        if @roster[grade]
+            @roster[grade].push(student_name)
+        else
+            @roster[grade] = Array.new.push(student_name)
+        end
+        @roster
+    end
+
+    def grade(level)
+        @roster[level]
+    end
+
+    def sort
+        @roster.each do |grade,student|
+            @roster[grade] = student.sort
+        end
+    end
+end
